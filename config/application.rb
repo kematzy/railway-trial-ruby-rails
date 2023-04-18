b047f3fd9e3e3ff18ef058a990716d943d662b47
Kematzy <kematzy@gmail.com>
2023-04-18 15:27:00 +0800

    Add ActionDispatch::HostAuthorization

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module RubyRailsPostgres
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.hosts << "rails-production-2ba1.up.railway.app"
   end
 end
